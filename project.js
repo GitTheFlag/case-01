bdb46532bde1394dd5444fde63ecb4848c45de3f
Dev1 <dev1@devteam.io>
2026-03-04 14:00:00 +0100

    GTF{l0g_sh0ws_3v3ryth1ng} feat: add farewell function and null check

--- project.js
+++ project.js
@@ -2,7 +2,12 @@
 // Status: work in progress
 
 function greet(name) {
+  if (!name) return "Hello, stranger!";
   return "Hello, " + name + "!";
 }
 
-module.exports = { greet };
+function farewell(name) {
+  return "Goodbye, " + name + "!";
+}
+
+module.exports = { greet, farewell };
